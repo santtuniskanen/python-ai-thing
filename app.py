c1aed70d4231aa6748fc390e66f3f729d80b52f0
santtuniskanen <santtuniska02@gmail.com>
2023-07-05 15:28:19 +0300

    imports

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-from flask import Flask, redirect, url_for, request, render_template, session
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
+import requests, os, uuid, json
+from dotenv import load_dotenv
+load_dotenv()
